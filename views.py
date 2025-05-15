770550cce232908c6744af016baca88658756ac2
Opago Pay <hours_07_stacks@icloud.com>
2025-05-15 19:35:47 +0200

    Merge more thoroughly - ALERT: We loose execute split

--- views.py
+++ views.py
@@ -1,17 +1,18 @@
-from fastapi import Depends, Request
-from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
-
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
+from lnbits.helpers import template_renderer
 
-from . import splitpayments_ext, splitpayments_renderer
-
-templates = Jinja2Templates(directory="templates")
+splitpayments_generic_router = APIRouter()
 
 
-@splitpayments_ext.get("/", response_class=HTMLResponse)
+def splitpayments_renderer():
+    return template_renderer(["splitpayments/templates"])
+
+
+@splitpayments_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
     return splitpayments_renderer().TemplateResponse(
-        "splitpayments/index.html", {"request": request, "user": user.dict()}
+        "splitpayments/index.html", {"request": request, "user": user.json()}
     )
